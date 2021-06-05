1f255eaaf6fc92d35565ca48a453130d590efdb4
diegocrocha <diego.rocha@bandtec.com.br>
2021-06-05 18:15:37 -0300

    init spring

---
+++ src/test/java/com/zup/desafiozup/DesafioZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.desafiozup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
